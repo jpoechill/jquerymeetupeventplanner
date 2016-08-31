3e851556d2a1a70940903d8e73177d8c4b77ff33
pogithub <poamrong@rith.com>
2016-08-31 14:49:12 -0700

    Found lost code via. index.html, recommitting

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,12 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function () {
-    // Do something here
-    console.log('Hello from Gulpfile.js!');
+    // Do soemthing here
+    console.log('Hello from Gulpfile.js!')
 });
 
-gulp.task('sass', function () {
- gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+var browserSync = require('browser-sync').create();
+browserSync.init({
+    server: "./"
 });
-
-// gulp.task('sass', function() {
-//     gulp.src('sass/**/*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('./css'));
-// });
+browserSync.stream();
