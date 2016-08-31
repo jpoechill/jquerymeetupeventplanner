a00ab990225d675ad10cf1d07db0016002372201
pogithub <poamrong@rith.com>
2016-08-31 15:24:07 -0700

    Re-initialized gulp streams

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'));
+});
 
 gulp.task('default', function () {
-    // Do soemthing here
-    console.log('Hello from Gulpfile.js!')
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
-
-var browserSync = require('browser-sync').create();
-browserSync.init({
-    server: "./"
-});
-browserSync.stream();
